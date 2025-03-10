e72e2811d0be2babcf72a2ddd5666f7de3ab364c
plasmaaShark <shaylinsmith07@gmail.com>
2025-03-10 12:22:53 -0700

    Set up db

--- Project_Code/Backend/app.py
+++ Project_Code/Backend/app.py
@@ -5,6 +5,7 @@ from Database import database
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -30,20 +31,17 @@ def register():
     # add it to the database and create a new user in the table
 
 # open the database connection when flask is opened
-@app.before_request
+@app.before_first_request
 def start_database():
     database.get_db()
     print("database open")
 
 # close the database connections when flask is closed
-@app.teardown_appcontext
-def close_database(exception):
+def close_database():
     database.close_db()
     print("database closed")
 
-@app.route('/')
-def start():
-    return "hello world"
+atexit.register(close_database)
 
 if __name__ == '__main__':
     app.run(debug=True)
