5daacf45e74667764fd207b0d9076e2d4dec9b06
j3erdman <j3erdman@gmail.com>
2025-04-02 19:33:26 -0700

    Create structure for CancelReservation.js

---
+++ Project_Code/Frontend/app/src/CancelReservation.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CancelReservation = () => {
+    return (
+        <div>
+          <h2>My Reservations</h2>
+        </div>
+      );
+}
+
+export default CancelReservation;
+
